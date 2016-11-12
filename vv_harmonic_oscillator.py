67d6f477fd0af6069f3fcb2b3251b45b89443135
RJ Nowling <rnowling@gmail.com>
2016-11-11 20:40:08 -0600

    Remove plotting from simulation scripts since it's been pulled out into its own script

--- vv_harmonic_oscillator.py
+++ vv_harmonic_oscillator.py
@@ -17,14 +17,6 @@ def parseargs():
                         required=True,
                         type=str)
 
-    parser.add_argument("--phase-fl",
-                        required=True,
-                        type=str)
-
-    parser.add_argument("--energy-fl",
-                        required=True,
-                        type=str)
-
     return parser.parse_args()
 
 
@@ -56,33 +48,6 @@ if __name__ == "__main__":
                  integrator,
                  (ts, xs, vs))
 
-    ts = np.array(ts)
-    vs = np.array(vs)
-    xs = np.array(xs)
-    
-    ke = 0.5 * mass * vs * vs
-    pe = 0.5 * mass * omega * omega * xs * xs
-    total_energy = ke + pe
-
-    plt.clf()
-    plt.plot(xs, vs)
-    plt.xlabel("Position", fontsize=16)
-    plt.ylabel("Velocity", fontsize=16)
-    plt.title("Phase Diagram", fontsize=18)
-    plt.savefig(args.phase_fl, DPI=300)
-
-    plt.clf()
-    plt.plot(ts, ke, "c-", label="Kinetic")
-    plt.plot(ts, pe, "m-", label="Potential")
-    plt.plot(ts, total_energy, "k-", label="Total")
-    plt.xlabel("Time (sec)", fontsize=16)
-    plt.ylabel("Energy (J)", fontsize=16)
-    plt.title("System Energy", fontsize=18)
-    min_energy = min(min(ke), min(pe), min(total_energy))
-    max_energy = max(max(ke), max(pe), max(total_energy))
-    plt.ylim([- max_energy * 0.2, max_energy * 1.2])
-    plt.legend(loc="lower right")
-    plt.savefig(args.energy_fl, DPI=300)
 
     
 
